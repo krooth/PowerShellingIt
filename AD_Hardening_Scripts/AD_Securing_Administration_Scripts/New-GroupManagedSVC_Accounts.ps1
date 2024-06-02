29f221f9d8d4a377fabbf945ed74cb78227bb02c
krooth <hermon.kidanew@gmail.com>
2024-06-02 22:55:18 +0300

    Solved Effective time comparision

--- AD_Hardening_Scripts/AD_Securing_Administration_Scripts/New-GroupManagedSVC_Accounts.ps1
+++ AD_Hardening_Scripts/AD_Securing_Administration_Scripts/New-GroupManagedSVC_Accounts.ps1
@@ -15,25 +15,48 @@ Step 2: Create a Security groups and add all the computers we'll use the GMSA on
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
-$sec_group_name = Read-Host "What it the name of gMSA password retrieving nodes securty group?"
-$path = Read-Host "Sepcify the full path of the security group to be created."
 
-$gMSA_Name = Read-Host "What is the name of the gmsa you wish to create?"
-$gMSA_FQDN = Read-Host "What is the DNS Name (Fully Qualified Domain Name), like (bob.acme.local)?"
 
 # Getting all the hostname from the group
 $gMSA_HostName = Get-ADGroup -Identity gMSAs | Select-Object -ExpandProperty Name
 
 #Functions
+# Function for recieving gmsa value inputs
+function gmsa_property_input {
+    try {
+        $gmsa_values = @()
+        
+        $gMSA_Name = Read-Host "What is the name of the gmsa you wish to create?"
+        # Check if the gMSA name is provided
+        if ([string]::IsNullOrWhiteSpace($gMSA_Name)) {
+            Write-Host "Please provide a valid name for the Group Managed Service Account (gMSA)." -ForegroundColor Red
+        } else {
+            $gMSA_FQDN = Read-Host "What is the DNS Name (Fully Qualified Domain Name), like (bob.acme.local)?"
+            if ([string]::IsNullOrWhiteSpace($gMSA_FQDN)) {
+                Write-Host "Please provide a valid name for the Group Managed Service Account (gMSA)." -ForegroundColor Red
+            } else {
+                $kerb_encryption_type = Read-Host "What kind of encyrption do you want to use(None, AES128, AES256, DES, RC4)?"
+        }
+        $kerb_encryption_type = $kerb_encryption_type.ToUpper()  
+        }
+    } catch {
+        Write-Host $_.Exception.Message
+    }
+}
+
 # Function for creating Security Group
 function create_group {
-    [CmdletBinding()]
-    param (
-        [string]$sec_group_name, [string]$path
-    )
+   try {
+    $sec_group_name = Read-Host "What it the name of gMSA password retrieving nodes securty group?"
+    $path = Read-Host "Sepcify the full path of the security group to be created."    
     
-    $ad_group = New-ADGroup $sec_group_name -Path $Path -GroupCategory Security -GroupScope DomainLocal -PassThru -Verbose
-    return $ad_group
+    # Create security group
+    $ad_group = New-ADGroup $sec_group_name -Path $Path -GroupCategory Security -GroupScope DomainLocal -PassThru -Verbose 
+    $sec_group_name = $ad_group | Select-Object -ExpandProperty SamAccountName
+    return $sec_group_name
+   } catch {
+    Write-Host $_.Exception.Message
+   } 
 }
 # Function to add nodes to the security group that can retreieve the credentials of the gMSA
 function add_nodes_to_sec_group {
@@ -42,18 +65,22 @@ function add_nodes_to_sec_group {
     param (
         [string]$sec_group_name
     )
-    $nodes = @()
+
+    try {
+        $nodes = @()
     do {
         $node = Read-Host "Enter the name of node to add to the security group?"
         if ($node -ne "") {
             $nodes += $node
-        } 
+        }
     } while (node -ne "")
     # Loop through each node name and add it to the security group
     foreach ($node in $nodes) {
         Add-ADGroup -Identity $sec_group_name -Members $node
+    }    
+    } catch {
+        Write-Host $_.Exception.Message
     }
-    
 }
 # Function for creating gMSA
 function create_gMSA() {
@@ -61,8 +88,30 @@ function create_gMSA() {
     param (
         [string]$gMSA_Name
     )
+    try {
+        New-ADServiceAccount $gMSA_Name -DNSHostName $gMSA_HostName -PrincipalsAllowedToRetrieveManagedPassword $sec_group_name -KerberosEncryptionType $   
+    } catch {
+        Write-Host $_.Exception.Message
+    }
 }
 
+
+function check_kds_rootkey_validity() {
+    try {
+        $thresholdDate = (Get-Date).AddDays(-1).ToString("yyyy/MM/dd hh:mm:ss tt")
+
+        # Get the KDS root keys created before the threshold date
+        $kdsRootKeys = Get-KdsRootKey | Where-Object { $_.EffectiveTime -lt $thresholdDate }
+        # Check if there are any KDS root keys created before the threshold date        
+        if ($kdsRootKeys.Count -gt 0) {
+            gmsa_property_input
+            $sec_group_name = create_group
+            create_gMSA($sec_group_name)
+        }       
+    } catch {
+        Write-Host $_.Exception.Message
+    }
+}
 # Check if an KDS root keys exist
 $rootKeys = Get-KdsRootKey
 if ($rootKeys) {
@@ -70,12 +119,13 @@ if ($rootKeys) {
     foreach (key in $rootKeys) {
         # Get the curent date and time
         $currentTime = Get-Date -Format "MM/dd/yyy HH:mm:ss tt"
+        
 
         # Check if "EffectiveTime" Property exists
         if ($key.PSPRopertyNames -contains "EffectiveTime") {
             $effectiveTime = $key.EffectiveTime
 
-            if ($effectiveTime)
+            if ($effectiveTime) 
         }
         else {
             Add-KdsRootKey -EffectiveImmediately
